24afe07da18dfb2ef78ebff651d0484415c816af
Mila <darlingmila@yandex.ru>
2024-01-24 18:14:59 +0300

    change symbols and test dates

--- 3-birthday/app.js
+++ 3-birthday/app.js
@@ -7,19 +7,24 @@ function isOldEnough(dateString) {
   if (dateToday.getFullYear() - birthdayDate.getFullYear() > 14) return true;
 
   // dateToday.getFullYear() === birthdayDate.getFullYear()
-  if (dateToday.getMonth() > birthdayDate.getMonth()) return false;
-  if (dateToday.getMonth() < birthdayDate.getMonth()) return true;
+  if (dateToday.getMonth() > birthdayDate.getMonth()) return true;
+  if (dateToday.getMonth() < birthdayDate.getMonth()) return false;
 
   // dateToday.getMonth() === birthdayDate.getMonth()
-  if (dateToday.getDate() > birthdayDate.getDate()) return false;
-  if (dateToday.getDate() < birthdayDate.getDate()) return true;
+  if (dateToday.getDate() > birthdayDate.getDate()) return true;
+  if (dateToday.getDate() < birthdayDate.getDate()) return false;
 
   // dateToday.getDate() === birthdayDate.getDate()
   return true;
 }
 
-console.log(isOldEnough("2009-12-21"));
-console.log(isOldEnough("2009-12-23"));
-console.log(isOldEnough("2009-12-20"));
-console.log(isOldEnough("2008-12-21"));
-console.log(isOldEnough("2010-01-21"));
+console.log("на 24.01.2024")
+console.log(isOldEnough("2009-12-21")); // true
+console.log(isOldEnough("2010-01-21")); // true
+console.log('---')
+console.log(isOldEnough("2010-03-21")); // false
+console.log(isOldEnough("2010-01-30")); // false
+console.log('---')
+console.log(isOldEnough("2010-01-23")); // true
+console.log(isOldEnough("2010-01-24")); // true
+console.log(isOldEnough("2010-01-25")); // false
